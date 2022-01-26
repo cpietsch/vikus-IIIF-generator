58ef9616261370e08578a5ce8e99aa3eb286bf94
Christopher Pietsch <cpietsch@gmail.com>
2022-01-26 14:49:09 +0100

    first working async prio queue crawler

--- scripts/playground.py
+++ scripts/playground.py
@@ -32,10 +32,11 @@ def clearCache():
 
 #clearCache()
 
-logging.basicConfig(level=logging.DEBUG, handlers=[RichHandler()])
+logging.basicConfig(level=logging.WARNING, handlers=[RichHandler()])
 logger = logging.getLogger('rich')
 
-url = "https://iiif.wellcomecollection.org/presentation/v3/collections/genres"
+# url = "https://iiif.wellcomecollection.org/presentation/v3/collections/genres"
+url = "https://iiif.wellcomecollection.org/presentation/collections/genres/Broadsides"
 
 async def getJson(url):
     retries = 5
@@ -75,27 +76,6 @@ def getLabel(data):
 
 default_verbose = True
 
-async def worker(name, queue):
-    while True:
-        # Get a "work item" out of the queue.
-        await asyncio.sleep(1)
-        priority, manifest = await queue.get()
-        logger.info("{} got {}".format(name, manifest.id))
-
-        # Sleep for the "sleep_for" seconds.
-        await manifest.load()
-        
-        if manifest.data.get('items', False):
-            for item in manifest.data.get('items'):
-                print("{} added {}".format(name, item.get('id')))
-                child = Manifest(url=item.get('id'), depth=manifest.depth+1)
-                queue.put(asyncio.ensure_future((priority+1, child)))
-                # manifest.children.append(child)
-
-        queue.task_done()
-        print(f'{name} {priority} {manifest.id}')
-
-
 class Manifest:
     def __init__(self, *args, **kwargs):
         self.id = kwargs.get('url', '')
@@ -108,6 +88,7 @@ class Manifest:
         self.parent = kwargs.get('parent', None)
         self.label = None
         self.tree =  kwargs.get('tree', None)
+        self.path = self.parent and self.id.replace(self.parent.id, '') or self.id
 
     async def load(self):
         if self.url:
@@ -117,10 +98,15 @@ class Manifest:
                 logger.warning("url {} does not match id {}".format(self.url, self.data.get('id')))
             self.id = self.data.get('id')
             self.label = getLabel(self.data)
+            self.type = self.data.get('type')
+
             if(self.tree is None):
-                self.tree = Tree(f":open_file_folder: [link {self.id}]{self.label}")
+                self.tree = Tree(f":open_file_folder: [link {self.id}]{self.id}")
             else:
-                self.tree = self.tree.add(f":open_file_folder: [link {self.id}]{self.label}")
+                emoji = self.type == 'Collection' and ':open_file_folder:' or ':framed_picture:'
+                self.tree = self.tree.add(f"{emoji} [link {self.id}]{self.id}")
+
+            return self
 
     async def loadChildren(self):
         if self.data.get('items', False) and not self.loaded:
@@ -135,53 +121,77 @@ class Manifest:
             await item.loadChildren()
 
 
+async def worker(name, queue):
+    while True:
+        # Get a "work item" out of the queue.
+        prio, manifest = await queue.get()
 
+        # Sleep for the "sleep_for" seconds.
+        await manifest.load()
 
+        if manifest.data.get('items', False):
+            for item in manifest.data.get('items'):
+                # print("{} added {}".format(name, item.get('id')))
+                child = Manifest(url=item.get('id'), depth=manifest.depth+1, tree=manifest.tree, parent = manifest)
+                manifest.children.append(child)
+                # print(item.get('type'))
+                if item.get('type') == 'Collection' or item.get('type') == 'Manifest':
+                    queue.put_nowait((prio + 1 + random.uniform(0, 1), child))
 
-# 'label': {'en': ['Works by Genre']}
-def getLabel(data):
-    labels = list(data.get("label").values())[0]
-    label = labels[0]
-    return label
+        # Notify the queue that the "work item" has been processed.
+        queue.task_done()
+
+        logger.info(f'prio: {prio} {manifest.label} done with {len(manifest.children)} children, {queue.qsize()} items left')
+
 
 async def main():
     # Create a queue that we will use to store our "workload".
-    # queue = asyncio.PriorityQueue()
-
-    # Generate random timings and put them into the queue.
+    queue = asyncio.PriorityQueue()
 
     manifestEntry = Manifest(url=url, debug=True)
-    await manifestEntry.load()
-    await manifestEntry.loadChildren()
-    await manifestEntry.loadDeep()
+    queue.put_nowait((0, manifestEntry))
+
+    tasks = []
+    for i in range(1):
+        task = asyncio.create_task(worker(f'worker-{i}', queue))
+        tasks.append(task)
+
+    await queue.join()
+
+    # Cancel our worker tasks.
+    for task in tasks:
+        task.cancel()
+    # Wait until all worker tasks are cancelled.
+    await asyncio.gather(*tasks, return_exceptions=True)
+
+    # await manifestEntry.load()
+    # await manifestEntry.loadChildren()
+    # await manifestEntry.loadDeep()
     # console.log(manifestEntry.data)
     print(manifestEntry.tree)
 
-    # await manifestEntry.loadChildren()
-
-    # await crawl(manifestEntry)
     print('Done')
 
 
 asyncio.run(main())
 
-async def crawl(manifest):
-    print("crawl {}".format(manifest.id))
-    await manifest.load()
+# async def crawl(manifest):
+#     print("crawl {}".format(manifest.id))
+#     await manifest.load()
     
-    logger.info(manifest.data, log_locals=True)
-    # await manifest.loadChildren()
+#     logger.info(manifest.data, log_locals=True)
+#     # await manifest.loadChildren()
 
-    # if manifest.data.get('items', False):
-    #         for item in manifest.data.get('items'):
-    #             print("added {}".format(item.get('id')))
-    #             child = Manifest(url=item.get('id'), depth=manifest.depth+1)
-    #             manifest.children.append(child)
-    #             if(manifest.depth < 2):
-    #                 await crawl(child)
-    #             # await asyncio.ensure_future(crawl(child))
+#     # if manifest.data.get('items', False):
+#     #         for item in manifest.data.get('items'):
+#     #             print("added {}".format(item.get('id')))
+#     #             child = Manifest(url=item.get('id'), depth=manifest.depth+1)
+#     #             manifest.children.append(child)
+#     #             if(manifest.depth < 2):
+#     #                 await crawl(child)
+#     #             # await asyncio.ensure_future(crawl(child))
 
-    return manifest
+#     return manifest
 
 
 # m = Manifest(url=url)
