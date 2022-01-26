7ad994a0a1b6b38f61add42ed66debc899d6db3f
Christopher Pietsch <cpietsch@gmail.com>
2022-01-26 19:37:39 +0100

    simple ImageDownloader

--- scripts/playground.py
+++ scripts/playground.py
@@ -10,6 +10,7 @@ import aiohttp
 import random
 import time
 import traceback
+import hashlib
 
 from rich import pretty
 from rich.logging import RichHandler
@@ -24,23 +25,55 @@ debug = True
 cache = redis.Redis(host='redis', port=6379)
 
 # clear redis cache
-cache.flushdb()
+# cache.flushdb()
 
-logging.basicConfig(level=logging.DEBUG, handlers=[RichHandler()])
+logging.basicConfig(level=logging.WARN, handlers=[RichHandler()])
 logger = logging.getLogger('rich')
 
 # url = "https://iiif.wellcomecollection.org/presentation/v3/collections/genres"
 # url = "https://iiif.wellcomecollection.org/presentation/collections/genres/Broadsides"
 url = "https://iiif.wellcomecollection.org/presentation/collections/genres/Myths_and_legends"
 
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        # return absolute path
+        return os.path.abspath(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+
+
+class ImageDownloader:
+    async def download(self, url, path):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    logger.info("downloading {}".format(url))
+                    data = await response.read()
+                    filename = hashlib.md5(data).hexdigest() + ".jpg"
+                    filepath = os.path.join(path, filename)
+                    with open(filepath, 'wb') as f:
+                        f.write(data)
+                    return filepath
+                else:
+                    return None
 
 async def main():
+
+    dataPath = createFolder("../data/images")
+    print(dataPath)
 
     crawler = Crawler(url=url, cache=cache, logger=logger, workers=1)
     manifest = await crawler.getManifest()
 
-    manifests = manifest.getFlatList(manifest)
-    thumbnails = [ manifest.getThumbnail() for manifest in manifests ]
+    manifestsFlat = manifest.getFlatList(manifest)
+    thumbnails = [ manifest.getThumbnail() for manifest in manifestsFlat ]
+
+    downloader = ImageDownloader()
+
+    for thumbnail in thumbnails:
+        await downloader.download(thumbnail, dataPath)
 
     print(manifest.tree)
     print(thumbnails)
