d1fc757fc70caca962e25d1ac9e7af5fc4949c87
Christopher Pietsch <cpietsch@gmail.com>
2022-06-02 14:53:03 +0200

    sharpsheet now accepts an array

--- scripts/sharpsheet.py
+++ scripts/sharpsheet.py
@@ -5,24 +5,27 @@ import os
 
 from click import command
 
+
 class Sharpsheet:
     def __init__(self, *args, **kwargs):
         self.logger = kwargs.get('logger', logging.getLogger('Sharpsheet'))
-    
-    async def generate(self, inputPath, *args, **kwargs):
-        self.logger.debug("generate spritesheet from {}".format(inputPath))
+
+    async def generate(self, files, *args, **kwargs):
+        self.logger.debug(
+            "generate spritesheet from {} files".format(len(files)))
         format = kwargs.get('format', 'jpg')
         quality = kwargs.get('quality', 60)
         dimension = kwargs.get('dimension', '2048')
-        outputPath = kwargs.get('outputPath', os.path.abspath(os.path.join(inputPath, '../sprites/')))
-        input = inputPath + '/*.jpg'
+        outputPath = kwargs.get('outputPath', None)
 
-        command = ['sharpsheet', input,
-            '--outputPath', outputPath,
-            '--outputFormat', format,
-            '--outputQuality', quality.__str__(),
-            '--sheetDimension', dimension.__str__()]
-        print(command)
+        command = ['/scripts/sharpsheet/bin/sharpsheet', files.__str__(),
+                   '--outputPath', outputPath,
+                   '--outputFormat', format,
+                   '--outputQuality', quality.__str__(),
+                   '--sheetDimension', dimension.__str__()]
+
+        print("command")
+        print(command.__str__())
 
         proc = await asyncio.create_subprocess_exec(
             *command,
@@ -34,11 +37,45 @@ class Sharpsheet:
         self.logger.info("stdout: {}".format(stdout.decode()))
 
         if stderr:
-            self.logger.error("error generating spritesheet: {}".format(stderr.decode()))
+            self.logger.error(
+                "error generating spritesheet: {}".format(stderr.decode()))
             return False
         else:
             self.logger.info("spritesheet generated")
             return True
+
+    async def generateFromPath(self, inputPath, *args, **kwargs):
+        self.logger.debug("generate spritesheet from {}".format(inputPath))
+        format = kwargs.get('format', 'jpg')
+        quality = kwargs.get('quality', 60)
+        dimension = kwargs.get('dimension', '2048')
+        outputPath = kwargs.get('outputPath', os.path.abspath(
+            os.path.join(inputPath, '../sprites/')))
+        input = inputPath + '/*.jpg'
+
+        command = ['sharpsheet', input,
+                   '--outputPath', outputPath,
+                   '--outputFormat', format,
+                   '--outputQuality', quality.__str__(),
+                   '--sheetDimension', dimension.__str__()]
+        print(command.__str__())
+
+        proc = await asyncio.create_subprocess_exec(
+            *command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+
+        stdout, stderr = await proc.communicate()
+
+        self.logger.info("stdout: {}".format(stdout.decode()))
+
+        if stderr:
+            self.logger.error(
+                "error generating spritesheet: {}".format(stderr.decode()))
+            return False
+        else:
+            self.logger.info("spritesheet generated")
+            return outputPath
 
 
 if __name__ == '__main__':
