52d5d9e8ab9ee641379683b4703172c03a3276c3
Christopher Pietsch <cpietsch@gmail.com>
2022-08-19 16:38:10 +0200

    lowered batch size for feature extraction and added cache skip

--- scripts/featureExtractor.py
+++ scripts/featureExtractor.py
@@ -10,6 +10,7 @@ from transformers import CLIPProcessor, CLIPModel, CLIPFeatureExtractor
 from rich.progress import track
 import os
 
+
 class FeatureExtractor:
     def __init__(self, **kwargs):
         self.model = None
@@ -18,7 +19,7 @@ class FeatureExtractor:
             'logger', logging.getLogger('FeatureExtractor'))
         self.cache = kwargs.get('cache', None)
         self.progress = kwargs.get('progress', None)
-        self.overwrite = kwargs.get('overwrite', False)
+        self.skipCache = kwargs.get('skipCache', False)
         self.instanceId = kwargs.get('instanceId', None)
 
     @torch.no_grad()
@@ -57,7 +58,7 @@ class FeatureExtractor:
 
     @torch.no_grad()
     async def get_features(self, id, image_path):
-        if(self.cache and not self.overwrite):
+        if(self.cache and not self.skipCache):
             features = await self.cache.getFeatures(id)
             if(features is not None):
                 self.logger.debug("Found features in cache for {}".format(id))
@@ -69,13 +70,13 @@ class FeatureExtractor:
         return features
 
     @torch.no_grad()
-    async def batch_extract_features_cached(self, imageList, batchSize=64):
+    async def batch_extract_features_cached(self, imageList, batchSize=16):
         featuresList = []
         idList = []
         queueList = []
         for (id, path) in imageList:
             features = await self.cache.getFeatures(id)
-            if(features is not None):
+            if(features is not None and not self.skipCache):
                 self.logger.debug("Found features in cache for {}".format(id))
                 featuresList.append(features)
                 idList.append(id)
@@ -88,13 +89,18 @@ class FeatureExtractor:
             (ids, features) = await self.batch_extract_features(queueList, batchSize)
             featuresList.extend(features)
             idList.extend(ids)
-            for (id, feature) in zip(ids, features):
-                self.logger.debug("Caching features for {}".format(id))
-                await self.cache.saveFeatures(id, feature)
+
+        if self.cache is not None:
+            await self.cache.postProgress(self.instanceId, {
+                'progress': 1,
+                'task': 'features',
+                'size': len(imageList),
+                'completed': len(featuresList)
+            })
         return (idList, featuresList)
 
     @torch.no_grad()
-    async def batch_extract_features(self, imageList, batchSize=64):
+    async def batch_extract_features(self, imageList, batchSize=16):
         self.logger.debug("Extracting features of {}".format(len(imageList)))
         features = []
         ids = []
@@ -103,7 +109,8 @@ class FeatureExtractor:
 
         batchedImageList = [imageList[i:i + batchSize]
                             for i in range(0, len(imageList), batchSize)]
-        for batch in track(batchedImageList, total=len(batchedImageList), description="Extracting features in batches"):
+        # for batch in track(batchedImageList, total=len(batchedImageList), description="Extracting features in batches"):
+        for batch in batchedImageList:
             images = [self.prepareImage(image_path)
                       for (id, image_path) in batch]
             inputs = self.processor(
@@ -114,7 +121,12 @@ class FeatureExtractor:
             ids.extend([id for (id, image_path) in batch])
 
             if self.cache is not None:
-                completed += len(batchedImageList)
+                for (id, feature) in zip(ids, features):
+                    self.logger.debug("Caching features for {}".format(id))
+                    await self.cache.saveFeatures(id, feature)
+
+            if self.cache is not None:
+                completed += len(batch)
                 await self.cache.postProgress(self.instanceId, {
                     'progress': completed / total,
                     'task': 'features',
@@ -140,7 +152,7 @@ class FeatureExtractor:
 
 
 async def main():
-    extractor = FeatureExtractor(overwrite=True)
+    extractor = FeatureExtractor(skipCache=True)
     extractor.load_model()
     testImagePath = 'files/test.png'
     features = extractor.extract_features(testImagePath)
