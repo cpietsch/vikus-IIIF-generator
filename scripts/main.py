b8c986585af29658d6cb96d6366e131f5d9b718c
Christopher Pietsch <cpietsch@gmail.com>
2023-04-20 15:39:17 +0200

    fixed delete_instance route causing a panic

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,6 @@
 from enum import IntEnum
 from typing import List, Union
 import asyncio
-from imp import reload
 import json
 import uuid
 from warnings import catch_warnings
@@ -23,7 +22,18 @@ import shutil
 import uuid
 from helpers import calculateThumbnailSize
 
-from vikus import create_config_json, crawlCollection, crawlImages, makeMetadata, makeSpritesheets, saveConfig, makeZip, makeFeatures, makeUmap, cache
+from vikus import (
+    create_config_json,
+    crawlCollection,
+    crawlImages,
+    makeMetadata,
+    makeSpritesheets,
+    saveConfig,
+    makeZip,
+    makeFeatures,
+    makeUmap,
+    cache,
+)
 from connectionManager import ConnectionManager
 
 LOGGER = logging.getLogger(__name__)
@@ -65,10 +75,13 @@ def home():
 @app.get("/instances")
 def list_instances():
     instances = []
-    paths = sorted(filter(os.path.isdir, Path(DATA_DIR).iterdir()),
-                   key=os.path.getmtime, reverse=True)
+    paths = sorted(
+        filter(os.path.isdir, Path(DATA_DIR).iterdir()),
+        key=os.path.getmtime,
+        reverse=True,
+    )
     for dir in paths:
-        if dir.name in ['images', 'viewer']:
+        if dir.name in ["images", "viewer"]:
             continue
         instance = {
             "id": dir.name,
@@ -99,7 +112,9 @@ def read_instance(instance_id: str):
         return {"error": "Instance {} doesn't exist".format(instance_id)}, 404
     configFile = os.path.join(path, "instance.json")
     if not os.path.isfile(configFile):
-        return {"error": "Instance {} doesn't have a config file".format(instance_id)}, 404
+        return {
+            "error": "Instance {} doesn't have a config file".format(instance_id)
+        }, 404
     with open(configFile, "r") as f:
         config = json.load(f)
     return config
@@ -108,12 +123,23 @@ def read_instance(instance_id: str):
 @app.post("/instances/steps/collection")
 async def crawl_collection(
     instance_id: str = Query(title="Instance ID", description="Instance ID"),
-    worker: int = Query(DEFAULTS["collection"]["worker"],
-                        title="Workers", description="Number of workers"),
-    depth: int = Query(DEFAULTS["collection"]["depth"], title="Depth",
-                       description="Recursive crawl depth", min=0, max=100),
+    worker: int = Query(
+        DEFAULTS["collection"]["worker"],
+        title="Workers",
+        description="Number of workers",
+    ),
+    depth: int = Query(
+        DEFAULTS["collection"]["depth"],
+        title="Depth",
+        description="Recursive crawl depth",
+        min=0,
+        max=100,
+    ),
     skip_cache: bool = Query(
-        DEFAULTS["collection"]["skip_cache"], title="Skip cache", description="Skip cache"),
+        DEFAULTS["collection"]["skip_cache"],
+        title="Skip cache",
+        description="Skip cache",
+    ),
 ):
     """
     Crawl a IIIF collection using parallel workers and a maximal depth.
@@ -123,11 +149,7 @@ async def crawl_collection(
         return config
 
     manifests = await crawlCollection(
-        config["iiif_url"],
-        instance_id,
-        worker,
-        depth,
-        skip_cache
+        config["iiif_url"], instance_id, worker, depth, skip_cache
     )
 
     config["collection"] = True
@@ -136,9 +158,7 @@ async def crawl_collection(
     if instance_id not in InstanceManager:
         InstanceManager[instance_id] = {"config": config}
 
-    InstanceManager[instance_id].update({
-        "manifests": manifests
-    })
+    InstanceManager[instance_id].update({"manifests": manifests})
 
     return config
 
@@ -146,16 +166,26 @@ async def crawl_collection(
 @app.post("/instances/steps/images")
 async def crawl_images(
     instance_id: str = Query(title="Instance ID", description="Instance ID"),
-    worker: int = Query(DEFAULTS["images"]["worker"],
-                        title="Workers", description="Number of workers"),
+    worker: int = Query(
+        DEFAULTS["images"]["worker"], title="Workers", description="Number of workers"
+    ),
     skip_cache: bool = Query(
-        DEFAULTS["images"]["skip_cache"], title="Skip cache", description="Skip cache")
+        DEFAULTS["images"]["skip_cache"], title="Skip cache", description="Skip cache"
+    ),
 ):
     """
     Download all thubmnail images from a IIIF collection.
     """
-    if instance_id not in InstanceManager or "manifests" not in InstanceManager[instance_id]:
-        await crawl_collection(instance_id, worker=DEFAULTS["collection"]["worker"], depth=DEFAULTS["collection"]["depth"], skip_cache=DEFAULTS["collection"]["skip_cache"])
+    if (
+        instance_id not in InstanceManager
+        or "manifests" not in InstanceManager[instance_id]
+    ):
+        await crawl_collection(
+            instance_id,
+            worker=DEFAULTS["collection"]["worker"],
+            depth=DEFAULTS["collection"]["depth"],
+            skip_cache=DEFAULTS["collection"]["skip_cache"],
+        )
 
     config = InstanceManager[instance_id]["config"]
 
@@ -166,9 +196,7 @@ async def crawl_images(
     config["numImages"] = len(images)
     saveConfig(config)
 
-    InstanceManager[instance_id].update({
-        "images": images
-    })
+    InstanceManager[instance_id].update({"images": images})
 
     return config
 
@@ -179,21 +207,31 @@ async def make_metadata(
     skip_cache: bool = Query(
         default=DEFAULTS["metadata"]["skip_cache"],
         title="Skip cache",
-        description="Skip read cache and save value to cache"
-    )
+        description="Skip read cache and save value to cache",
+    ),
 ):
     """
     Create a metadata file for all images in a IIIF collection.
     Use Spacy to extract keywords.
     """
     # print(extract_keywords)
-    if instance_id not in InstanceManager or "manifests" not in InstanceManager[instance_id]:
-        await crawl_collection(instance_id, worker=DEFAULTS["collection"]["worker"], depth=DEFAULTS["collection"]["depth"], skip_cache=DEFAULTS["collection"]["skip_cache"])
+    if (
+        instance_id not in InstanceManager
+        or "manifests" not in InstanceManager[instance_id]
+    ):
+        await crawl_collection(
+            instance_id,
+            worker=DEFAULTS["collection"]["worker"],
+            depth=DEFAULTS["collection"]["depth"],
+            skip_cache=DEFAULTS["collection"]["skip_cache"],
+        )
 
     config = InstanceManager[instance_id]["config"]
     manifests = InstanceManager[instance_id]["manifests"]
 
-    metadata = await makeMetadata(manifests, instance_id, config["path"], skip_cache=skip_cache)
+    metadata = await makeMetadata(
+        manifests, instance_id, config["path"], skip_cache=skip_cache
+    )
 
     config["metadata"] = True
     config["metadataFile"] = metadata["file"]
@@ -211,15 +249,24 @@ async def make_spritesheets(
     """
     Create a spritesheet for all images in a IIIF collection given the downloaded thumbnails.
     """
-    if instance_id not in InstanceManager or "images" not in InstanceManager[instance_id]:
-        await crawl_images(instance_id, worker=DEFAULTS["images"]["worker"], skip_cache=DEFAULTS["images"]["skip_cache"])
+    if (
+        instance_id not in InstanceManager
+        or "images" not in InstanceManager[instance_id]
+    ):
+        await crawl_images(
+            instance_id,
+            worker=DEFAULTS["images"]["worker"],
+            skip_cache=DEFAULTS["images"]["skip_cache"],
+        )
 
     config = InstanceManager[instance_id]["config"]
     images = InstanceManager[instance_id]["images"]
     spriteSize = calculateThumbnailSize(len(images))
     # files = [os.path.abspath(path) for (id, path) in images]
 
-    await makeSpritesheets(images, instance_id, config["path"], config["spritesheetPath"], spriteSize)
+    await makeSpritesheets(
+        images, instance_id, config["path"], config["spritesheetPath"], spriteSize
+    )
 
     config["spritesheets"] = True
     saveConfig(config)
@@ -229,35 +276,37 @@ async def make_spritesheets(
 
 @app.post("/instances/steps/features")
 async def make_features(
-    instance_id: str = Query(
-        title="Instance ID",
-        description="Instance ID"
-    ),
+    instance_id: str = Query(title="Instance ID", description="Instance ID"),
     batch_size: int = Query(
         default=DEFAULTS["features"]["batch_size"],
         title="Batch Size",
-        description="Batch size for the feature extraction"
+        description="Batch size for the feature extraction",
     ),
     skip_cache: bool = Query(
         default=DEFAULTS["features"]["skip_cache"],
         title="Skip cache",
-        description="Skip read cache and save value to cache"
-    )
+        description="Skip read cache and save value to cache",
+    ),
 ):
     """
     Create a CLIP features fo all images in a IIIF collection given the downloaded thumbnails.
     """
-    if instance_id not in InstanceManager or "images" not in InstanceManager[instance_id]:
-        await crawl_images(instance_id, worker=DEFAULTS["images"]["worker"], skip_cache=DEFAULTS["images"]["skip_cache"])
+    if (
+        instance_id not in InstanceManager
+        or "images" not in InstanceManager[instance_id]
+    ):
+        await crawl_images(
+            instance_id,
+            worker=DEFAULTS["images"]["worker"],
+            skip_cache=DEFAULTS["images"]["skip_cache"],
+        )
 
     config = InstanceManager[instance_id]["config"]
     images = InstanceManager[instance_id]["images"]
 
     features = await makeFeatures(images, instance_id, batch_size, skip_cache)
 
-    InstanceManager[instance_id].update({
-        "features": features
-    })
+    InstanceManager[instance_id].update({"features": features})
     config["features"] = True
     saveConfig(config)
 
@@ -268,22 +317,46 @@ async def make_features(
 async def make_umap(
     instance_id: str = Query(title="Instance ID", description="Instance ID"),
     n_neighbors: int = Query(
-        DEFAULTS["umap"]["n_neighbors"], title="Neighbors", description="Number of neighbors"),
+        DEFAULTS["umap"]["n_neighbors"],
+        title="Neighbors",
+        description="Number of neighbors",
+    ),
     min_distance: float = Query(
-        DEFAULTS["umap"]["min_distance"], title="Min Distance", description="Minimum distance"),
+        DEFAULTS["umap"]["min_distance"],
+        title="Min Distance",
+        description="Minimum distance",
+    ),
     raster_fairy: bool = Query(
-        DEFAULTS["umap"]["raster_fairy"], title="Raster Fairy", description="Use raster fairy"),
+        DEFAULTS["umap"]["raster_fairy"],
+        title="Raster Fairy",
+        description="Use raster fairy",
+    ),
 ):
     """
     Create a UMAP embedding based on the CLIP features.
     """
-    if instance_id not in InstanceManager or "features" not in InstanceManager[instance_id]:
-        await make_features(instance_id, batch_size=DEFAULTS["features"]["batch_size"], skip_cache=DEFAULTS["features"]["skip_cache"])
+    if (
+        instance_id not in InstanceManager
+        or "features" not in InstanceManager[instance_id]
+    ):
+        await make_features(
+            instance_id,
+            batch_size=DEFAULTS["features"]["batch_size"],
+            skip_cache=DEFAULTS["features"]["skip_cache"],
+        )
 
     config = InstanceManager[instance_id]["config"]
     (ids, features) = InstanceManager[instance_id]["features"]
 
-    await makeUmap(features, instance_id, config["path"], ids, n_neighbors, min_distance, raster_fairy)
+    await makeUmap(
+        features,
+        instance_id,
+        config["path"],
+        ids,
+        n_neighbors,
+        min_distance,
+        raster_fairy,
+    )
 
     config["similarity"] = True
     saveConfig(config)
@@ -318,11 +391,29 @@ async def run(instance_id: str = Query(title="Instance ID", description="Instanc
     """
     Run all steps of the pipeline for a given instance.
     """
-    await crawl_collection(instance_id, worker=DEFAULTS["collection"]["worker"], depth=DEFAULTS["collection"]["depth"], skip_cache=DEFAULTS["collection"]["skip_cache"])
-    await crawl_images(instance_id, worker=DEFAULTS["images"]["worker"], skip_cache=DEFAULTS["images"]["skip_cache"])
+    await crawl_collection(
+        instance_id,
+        worker=DEFAULTS["collection"]["worker"],
+        depth=DEFAULTS["collection"]["depth"],
+        skip_cache=DEFAULTS["collection"]["skip_cache"],
+    )
+    await crawl_images(
+        instance_id,
+        worker=DEFAULTS["images"]["worker"],
+        skip_cache=DEFAULTS["images"]["skip_cache"],
+    )
     await make_spritesheets(instance_id)
-    await make_features(instance_id, batch_size=DEFAULTS["features"]["batch_size"], skip_cache=DEFAULTS["features"]["skip_cache"])
-    await make_umap(instance_id, n_neighbors=DEFAULTS["umap"]["n_neighbors"], min_distance=DEFAULTS["umap"]["min_distance"], raster_fairy=DEFAULTS["umap"]["raster_fairy"])
+    await make_features(
+        instance_id,
+        batch_size=DEFAULTS["features"]["batch_size"],
+        skip_cache=DEFAULTS["features"]["skip_cache"],
+    )
+    await make_umap(
+        instance_id,
+        n_neighbors=DEFAULTS["umap"]["n_neighbors"],
+        min_distance=DEFAULTS["umap"]["min_distance"],
+        raster_fairy=DEFAULTS["umap"]["raster_fairy"],
+    )
     await make_metadata(instance_id, skip_cache=DEFAULTS["metadata"]["skip_cache"])
     await make_zip(instance_id)
 
@@ -332,7 +423,7 @@ async def run(instance_id: str = Query(title="Instance ID", description="Instanc
 
 
 @app.delete("/instances/{instance_id}")
-def delete_instance(instance_id: str = Query(title="Instance ID", description="Instance ID")):
+def delete_instance(instance_id: str):
     """
     Delete an instance.
     """
@@ -343,7 +434,12 @@ def delete_instance(instance_id: str = Query(title="Instance ID", description="I
         return {"error": "Instance {} doesn't exist".format(instance_id)}, 404
     else:
         shutil.rmtree(path)
-    return {"id": instance_id, "absolutePath": path, "label": instance_id, "status": "deleted"}
+    return {
+        "id": instance_id,
+        "absolutePath": path,
+        "label": instance_id,
+        "status": "deleted",
+    }
 
 
 @app.get("/defaults")
@@ -365,12 +461,18 @@ def set_defaults(defaults: dict):
         if key not in DEFAULTS:
             return {"error": "Key {} is not in defaults".format(key)}, 400
         if type(defaults[key]) != type(DEFAULTS[key]):
-            return {"error": "Type of key {} is not the same as in defaults".format(key)}, 400
+            return {
+                "error": "Type of key {} is not the same as in defaults".format(key)
+            }, 400
         for subkey in defaults[key]:
             if subkey not in DEFAULTS[key]:
                 return {"error": "Subkey {} is not in defaults".format(subkey)}, 400
             if type(defaults[key][subkey]) != type(DEFAULTS[key][subkey]):
-                return {"error": "Type of subkey {} is not the same as in defaults".format(subkey)}, 400
+                return {
+                    "error": "Type of subkey {} is not the same as in defaults".format(
+                        subkey
+                    )
+                }, 400
     DEFAULTS = defaults
 
     return DEFAULTS
@@ -392,24 +494,26 @@ async def websocket_endpoint(websocket: WebSocket, instance_id: str):
 
             if resp:
                 key, messages = resp[0]
-                for (id, message) in messages:
+                for id, message in messages:
                     last_id = id
-                    data = {key.decode(): val.decode()
-                            for key, val in message.items()}
+                    data = {key.decode(): val.decode() for key, val in message.items()}
                     await manager.broadcast(data)
             else:
                 await asyncio.sleep(0.1)
-                await manager.broadcast({"type": "ping", "data": 1, "instance": instance_id})
+                await manager.broadcast(
+                    {"type": "ping", "data": 1, "instance": instance_id}
+                )
     except Exception as e:
         manager.disconnect(websocket)
         print("Disconnected", instance_id)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",
-                host="0.0.0.0",
-                port=5000,
-                reload=True,
-                # log_level="debug"
-                )
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=5000,
+        reload=True,
+        # log_level="debug"
+    )
     # asyncio.run(app())
